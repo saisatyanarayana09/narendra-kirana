c17da709160f3cbaf16a357158e6490b373b117c
sai <saisatyanarayana2004@gmail.com>
2026-08-21 08:46:35 +0530

    UI: Upgrade font to Plus Jakarta Sans for a premium crisp look

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -7,7 +7,7 @@ export default {
   theme: {
     extend: {
       fontFamily: {
-        sans: ['Outfit', 'sans-serif'],
+        sans: ['"Plus Jakarta Sans"', 'sans-serif'],
       },
       colors: {
         primary: {
