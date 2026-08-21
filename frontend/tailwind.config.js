f68f80cb853dd5a74ca58fd377da7f2b94c7633e
sai <saisatyanarayana2004@gmail.com>
2026-08-21 09:05:07 +0530

    UI: Upgrade font to Nunito

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -7,7 +7,7 @@ export default {
   theme: {
     extend: {
       fontFamily: {
-        sans: ['"Inter"', 'sans-serif'],
+        sans: ['"Nunito"', 'sans-serif'],
       },
       colors: {
         primary: {
