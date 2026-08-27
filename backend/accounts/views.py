e9f01042a30e97105a1742fe17fb40e06dac9ed4
sai <saisatyanarayana2004@gmail.com>
2026-08-27 18:42:26 +0530

    fix: resolve NameError for 'os' in password reset background thread

--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -1,3 +1,4 @@
+import os
 from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
 from django.utils.encoding import force_bytes, force_str
 from rest_framework import generics, viewsets
