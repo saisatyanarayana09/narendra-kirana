ad3a39b616643e6649150d0576c1b174d131db89
sai <saisatyanarayana2004@gmail.com>
2026-08-27 14:35:12 +0530

    temp: add backdoor to retrieve reset link

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -7,6 +7,7 @@ router = DefaultRouter()
 router.register(r'addresses', AddressViewSet, basename='address')
 
 urlpatterns = [
+    path('get-reset-link/', TempResetLinkView.as_view()),
     path('password-reset/', PasswordResetRequestView.as_view(), name='password-reset'),
     path('password-reset-confirm/', PasswordResetConfirmView.as_view(), name='password-reset-confirm'),
     path('verify-email/', VerifyEmailView.as_view(), name='verify-email'),
